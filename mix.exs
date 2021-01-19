192ab3ddffcf4aec8c55fcdf307c4c56c7a7c549
Michał Śledź <sledzson08@gmail.com>
2021-01-19 17:56:00 +0100

    Initial commit

---
+++ mix.exs
@@ -0,0 +1,65 @@
+defmodule Membrane.Template.Mixfile do
+  use Mix.Project
+
+  @version "0.1.0"
+  @github_url "https://github.com/membraneframework/membrane_template_plugin"
+
+  def project do
+    [
+      app: :membrane_template_plugin,
+      version: @version,
+      elixir: "~> 1.10",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+
+      # hex
+      description: "Template Plugin for Membrane Multimedia Framework",
+      package: package(),
+
+      # docs
+      name: "Membrane Template plugin",
+      source_url: @github_url,
+      homepage_url: "https://membraneframework.org",
+      docs: docs()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: []
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_env), do: ["lib"]
+
+  defp deps do
+    [
+      {:membrane_core, "~> 0.6.0"},
+      {:ex_doc, "~> 0.22", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
+      {:credo, "~> 1.4", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Membrane Team"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => @github_url,
+        "Membrane Framework Homepage" => "https://membraneframework.org"
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md", "LICENSE"],
+      source_ref: "v#{@version}",
+      nest_modules_by_prefix: [Membrane.Template]
+    ]
+  end
+end
