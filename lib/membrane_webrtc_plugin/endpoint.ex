ff40ec8b771bf908905d570633220469d25cd298
Michał Śledź <michal.sledz@swmansion.com>
2021-04-19 12:31:18 +0200

    Don't divide tracks in Endpoint into audio and video

--- lib/membrane_webrtc_plugin/endpoint.ex
+++ lib/membrane_webrtc_plugin/endpoint.ex
@@ -10,57 +10,33 @@ defmodule Membrane.WebRTC.Endpoint do
   @type t :: %__MODULE__{
           id: id(),
           type: :participant | :screensharing,
-          tracks: %{
-            audio_tracks: %{Track.id() => [Track.t()]},
-            video_tracks: %{Track.id() => [Track.t()]}
-          }
+          inbound_tracks: %{Track.id() => Track.t()}
         }
 
-  defstruct id: nil, type: :participant, tracks: %{audio_tracks: %{}, video_tracks: %{}}
+  defstruct id: nil, type: :participant, inbound_tracks: %{}
 
-  @spec new(id :: id(), type :: type(), tracks :: [Track.t()]) :: Endpoint.t()
-  def new(id, type, tracks) do
-    {audio_tracks, video_tracks} =
-      Enum.reduce(tracks, {%{}, %{}}, fn
-        %Track{id: id, type: :audio} = track, {audio_tracks, video_tracks} ->
-          {Map.put(audio_tracks, id, track), video_tracks}
-
-        %Track{id: id, type: :video} = track, {audio_tracks, video_tracks} ->
-          {audio_tracks, Map.put(video_tracks, id, track)}
-      end)
-
-    tracks = %{audio_tracks: audio_tracks, video_tracks: video_tracks}
-    %__MODULE__{id: id, type: type, tracks: tracks}
+  @spec new(id :: id(), type :: type(), inbound_tracks :: [Track.t()]) :: Endpoint.t()
+  def new(id, type, inbound_tracks) do
+    inbound_tracks = Map.new(inbound_tracks, &{&1.id, &1})
+    %__MODULE__{id: id, type: type, inbound_tracks: inbound_tracks}
   end
 
-  @spec get_audio_tracks(endpoint :: Endpoint.t()) :: [Track]
-  def get_audio_tracks(endpoint), do: Map.values(endpoint.tracks.audio_tracks)
+  @spec get_audio_tracks(endpoint :: Endpoint.t()) :: [Track.t()]
+  def get_audio_tracks(endpoint),
+    do: Map.values(endpoint.inbound_tracks) |> Enum.filter(&(&1.type == :audio))
 
-  @spec get_video_tracks(ednpoint :: Endpoint.t()) :: [Track]
-  def get_video_tracks(endpoint), do: Map.values(endpoint.tracks.video_tracks)
+  @spec get_video_tracks(endpoint :: Endpoint.t()) :: [Track.t()]
+  def get_video_tracks(endpoint),
+    do: Map.values(endpoint.inbound_tracks) |> Enum.filter(&(&1.type == :video))
 
-  @spec get_track_by_id(endpoint :: Endpoint.t(), id :: Track.id()) :: Track | nil
-  def get_track_by_id(endpoint, id) do
-    endpoint.tracks.audio_tracks[id] || endpoint.tracks.video_tracks[id]
-  end
+  @spec get_track_by_id(endpoint :: Endpoint.t(), id :: Track.id()) :: Track.t() | nil
+  def get_track_by_id(endpoint, id), do: endpoint.inbound_tracks[id]
 
-  @doc """
-  Gets all tracks.
-  """
-  @spec get_tracks(endpoint :: Endpoint.t()) :: [Track]
-  def get_tracks(endpoint),
-    do: Map.values(endpoint.tracks.audio_tracks) ++ Map.values(endpoint.tracks.video_tracks)
+  @spec get_tracks(endpoint :: Endpoint.t()) :: [Track.t()]
+  def get_tracks(endpoint), do: Map.values(endpoint.inbound_tracks)
 
   @spec update_track_encoding(endpoint :: Endpoint.t(), track_id :: Track.id(), encoding :: atom) ::
           Endpoint.t()
-  def update_track_encoding(endpoint, track_id, value) do
-    cond do
-      Map.has_key?(endpoint.tracks.audio_tracks, track_id) ->
-        endpoint.tracks.audio_tracks
-        update_in(endpoint.tracks.audio_tracks[track_id], &%Track{&1 | encoding: value})
-
-      Map.has_key?(endpoint.tracks.video_tracks, track_id) ->
-        update_in(endpoint.tracks.video_tracks[track_id], &%Track{&1 | encoding: value})
-    end
-  end
+  def update_track_encoding(endpoint, track_id, value),
+    do: update_in(endpoint.inbound_tracks[track_id], &%Track{&1 | encoding: value})
 end
