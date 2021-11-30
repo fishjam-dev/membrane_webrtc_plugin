60e29e79c1ccbbea3b8b986aa5564f44c54c4510
Jakub Balinski <balinski.jakub@gmail.com>
2021-11-30 12:37:34 +0100

    bump ExSDP

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Membrane.WebRTC.Plugin.Mixfile do
       {:membrane_core, "~> 0.8.0", override: true},
       {:qex, "~> 0.5.0"},
       {:bunch, "~> 1.3.0"},
-      {:ex_sdp, github: "membraneframework/ex_sdp"},
+      {:ex_sdp, "~> 0.6.0"},
       {:membrane_rtp_format, "~> 0.3.0"},
       {:membrane_funnel_plugin, "~> 0.2.0"},
       {:membrane_h264_ffmpeg_plugin, "~> 0.12.0"},
