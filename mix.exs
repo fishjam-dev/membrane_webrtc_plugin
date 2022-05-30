7216e503d59df0a6fefc8da2ccbd94315e8b461c
Michał Śledź <michalsledz34@gmail.com>
2022-05-30 14:35:12 +0200

    Bump version to 0.5.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.WebRTC.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.4.0"
+  @version "0.5.0"
   @github_url "https://github.com/membraneframework/membrane_webrtc_plugin"
 
   def project do
