0a2dd39311ce5d2af8ae16b0f3cf1d313e089f9f
Michał Śledź <michalsledz34@gmail.com>
2022-05-30 14:36:48 +0200

    Fix LICENSE to be recognized by SPDX

--- mix.exs
+++ mix.exs
@@ -69,7 +69,7 @@ defmodule Membrane.WebRTC.Plugin.Mixfile do
   defp package do
     [
       maintainers: ["Membrane Team"],
-      licenses: ["Apache 2.0"],
+      licenses: ["Apache-2.0"],
       links: %{
         "GitHub" => @github_url,
         "Membrane Framework Homepage" => "https://membraneframework.org"
