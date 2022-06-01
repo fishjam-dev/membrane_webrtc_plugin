008ab1846d4693773114f4611bb831d53d1e4920
Michał Śledź <michalsledz34@gmail.com>
2022-06-01 14:54:06 +0200

    Update deps. Bump version to 0.6.0

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ The package can be installed by adding `membrane_webrtc_plugin` to your list of 
 ```elixir
 def deps do
   [
-    {:membrane_webrtc_plugin, "~> 0.5.0"}
+    {:membrane_webrtc_plugin, "~> 0.6.0"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.WebRTC.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.5.0"
+  @version "0.6.0"
   @github_url "https://github.com/membraneframework/membrane_webrtc_plugin"
 
   def project do
@@ -45,15 +45,15 @@ defmodule Membrane.WebRTC.Plugin.Mixfile do
       {:membrane_core, "~> 0.10.0"},
       {:qex, "~> 0.5.0"},
       {:bunch, "~> 1.3.0"},
-      {:ex_sdp, github: "membraneframework/ex_sdp"},
-      {:membrane_rtp_plugin, github: "membraneframework/membrane_rtp_plugin"},
-      {:membrane_rtp_format, github: "membraneframework/membrane_rtp_format"},
-      {:membrane_ice_plugin, github: "membraneframework/membrane_ice_plugin"},
+      {:ex_sdp, "~> 0.7.0"},
+      {:membrane_rtp_plugin, "~> 0.14.0"},
+      {:membrane_rtp_format, "~> 0.5.0"},
+      {:membrane_ice_plugin, "~> 0.12.0"},
       {:membrane_funnel_plugin, "~> 0.6.0"},
       {:membrane_h264_ffmpeg_plugin, "~> 0.21.0"},
-      {:membrane_rtp_vp8_plugin, github: "membraneframework/membrane_rtp_vp8_plugin"},
-      {:membrane_rtp_h264_plugin, github: "membraneframework/membrane_rtp_h264_plugin"},
-      {:membrane_rtp_opus_plugin, github: "membraneframework/membrane_rtp_opus_plugin"},
+      {:membrane_rtp_vp8_plugin, "~> 0.6.0"},
+      {:membrane_rtp_h264_plugin, "~> 0.13.0"},
+      {:membrane_rtp_opus_plugin, "~> 0.6.0"},
       {:ex_libsrtp, "~> 0.4.0"},
       {:ex_doc, "~> 0.22", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
